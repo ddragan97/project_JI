7e9bfd6e5aa406e9e394f06c5f1bebe9d9c9b4c9
ddragan97 <62421372+ddragan97@users.noreply.github.com>
2020-04-26 11:23:58 +0200

    Change require_once

--- Projekt_JI_w58276/ProjektJI_w58276/rejestracja.php
+++ Projekt_JI_w58276/ProjektJI_w58276/rejestracja.php
@@ -16,63 +16,8 @@
         <div id="frame">
             <?php
                 require_once("header.php");
+                require_once("menu.php");
             ?>
-            <nav>
-                <ul id="navUl">
-                    <li><a class="navLiA" href="index.php">Strona główna</a></li>
-                    <li><a class="navLiA" href="szukaj.php">Szukaj</a></li>
-                    <li><a id="iolog" class="navLiA" href="login.php"></a></li>
-                    <li><a id="iorej" class="navLiA" href="rejestracja.php">Rejestracja</a></li>
-                    <?php
-                        if(isset($_SESSION["czy"]))
-                        {
-                            echo '
-                            <li id="liLogin"><a class="navLiA" href="dodaj.php">Dodatki</a></li>
-                            ';
-                        }
-                    ?>
-                </ul>
-                
-                <?php
-                    if(isset($_SESSION["czy"]))
-                        {
-                ?>
-
-                            <script>
-
-                                var logIO = document.getElementById("iolog");
-                                logIO.text="Wyloguj";
-                                logIO.href="wyloguj.php";
-                                logIO.style.backgroundColor="#333366";
-
-                                var rejIO = document.getElementById("iorej");
-                                rejIO.style.visibility="hidden";
-
-                            </script>
-
-                <?php
-                        };
-
-                        if(!isset($_SESSION["czy"]))
-                        {
-
-                ?>
-                            <script>
-
-                                var logIO = document.getElementById("iolog");
-                                logIO.text="Zaloguj";
-                                logIO.href="login.php";
-                                logIO.style.backgroundColor="#424242";
-
-                                var rejIO = document.getElementById("iorej");
-                                rejIO.style.visibility="visible";
-
-                            </script>
-                <?php
-                        };
-                ?>
-
-            </nav>
             <main>
                 <form id="formRejestr" action="rej.php" method="POST">
                     <h1>Formularz rejestracyjny</h1>
